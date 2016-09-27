2b996da4830bb329c2e16664b343054accea767f
Tom Ritchford <tom@swirly.com>
2016-09-27 18:54:48 -0400

    Start to remove Manager...

--- src/timedata_visualizer/juce/communication.pyx
+++ src/timedata_visualizer/juce/communication.pyx
@@ -4,11 +4,6 @@ cdef extern from "<timedata_visualizer/juce/JApplication_inl.h>" namespace "time
     void quitJuceApplication()
 
 
-class JuceManager(multiprocessing.managers.BaseManager):
-    pass
-
-JuceManager.register('LightWindowProxy', _LightWindow)
-
 # _FROM_JUCE has to be a global variable so that _send_from_juce can be
 # a pure C function.  This gets assigned in each Juce process, but not in the
 # master process.
@@ -20,17 +15,18 @@ cdef void _send_from_juce(string s) with gil:
 
 
 @functools.singledispatch
-def _route_message_to_juce(object msg, object manager):
+def _route_message_to_juce(object msg):
     raise TypeError('Don\'t understand %s' % msg)
 
 
 @_route_message_to_juce.register(_LightWindowDesc)
-def _(_LightWindowDesc desc, object manager):
-    _RECEIVE(manager.LightWindowProxy(desc))
+def _(_LightWindowDesc desc):
+    # TODO:  if there's nothing here, the window fails to display!
+    print('_LightWindowDesc!')
 
 
 @_route_message_to_juce.register(str)
-def _(string s, object manager):
+def _(string s):
     if s == b'quit':
         quitJuceApplication()
     else:
@@ -38,13 +34,12 @@ def _(string s, object manager):
 
 
 cpdef _handle_juce_queue(send):
-    with JuceManager() as manager:
-        while True:
-            try:
-                item = send.get(timeout=_TIMEOUT)
-            except queue.Empty:
-                continue
-            _route_message_to_juce(item, manager)
+    while True:
+        try:
+            item = send.get(timeout=_TIMEOUT)
+        except queue.Empty:
+            continue
+        _route_message_to_juce(item)
 
 
 def _start_communication(send, receive):
