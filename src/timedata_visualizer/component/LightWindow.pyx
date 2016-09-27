98ba965f6528141de3b0d12431f023184c1187b8
Tom Ritchford <tom@swirly.com>
2016-09-27 14:21:46 -0400

    Now LightWindow handles its own memory and returns it to you.

--- src/timedata_visualizer/component/LightWindow.pyx
+++ src/timedata_visualizer/component/LightWindow.pyx
@@ -1,4 +1,4 @@
-import ctypes
+import ctypes, multiprocessing.sharedctypes
 
 cdef extern from "timedata_visualizer/component/LightWindow_inl.h" namespace "timedata":
     cdef cppclass LightWindow:
@@ -17,9 +17,12 @@ cdef class _LightWindow:
     cpdef void set_desc(self, _LightWindowDesc desc):
         self.cdata.setDesc(desc.cdata)
 
-    cpdef void set_lights(self, size_t width, size_t height, object data):
-        buffer = ctypes.addressof(data)
+    cpdef object set_dimensions(self, size_t width, size_t height):
+        cdef size_t size = 3 * width * height
+        self.array = multiprocessing.sharedctypes.RawArray(ctypes.c_uint8, size)
+        buffer = ctypes.addressof(self.array)
         self.cdata.setLights(width, height, <uint8_t*>(buffer))
+        return self.array
 
     cpdef void write_snapshot_to_file(self, string name):
         self.cdata.writeSnapshotToFile(name)
